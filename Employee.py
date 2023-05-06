422898f4f1b2921749bd7471b7b544cc3ca62557
Sohazur <mdsohazurislam@gmail.com>
2023-05-06 08:57:51 +0400

    Added Employee class

---
+++ Employee.py
@@ -0,0 +1,7 @@
+class Employee:
+    def __init__(self, name, id_number, department, job_title, basic_salary):
+        self.name = name
+        self.id_number = id_number
+        self.department = department
+        self.job_title = job_title
+        self.basic_salary = basic_salary
