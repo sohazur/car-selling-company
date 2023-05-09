22c1c7e62b3994115aaaeaf31eec58836e5c3576
Sohazur <mdsohazurislam@gmail.com>
2023-05-10 01:19:08 +0400

    Added setters, getters to Sale

--- Sale.py
+++ Sale.py
@@ -1,6 +1,29 @@
 class Sale:
     def __init__(self, salesperson, car, sale_price):
-        self.salesperson = salesperson
-        self.car = car
-        self.sale_price = sale_price
-        self.profit = sale_price - car.price
+        self.__salesperson = salesperson
+        self.__car = car
+        self.__sale_price = sale_price
+
+    # Getters
+    def get_sale_price(self):
+        return self.__sale_price
+
+    def get_car(self):
+        return self.__car
+
+    def get_salesperson(self):
+        return self.__salesperson
+
+    def get_profit(self):
+        # Calculate profit
+        return self.__sale_price - self.__car.get_price()
+
+    # Setters
+    def set_sale_price(self, sale_price):
+        self.__sale_price = sale_price
+
+    def set_car(self, car):
+        self.__car = car
+
+    def set_salesperson(self, salesperson):
+        self.__salesperson = salesperson
